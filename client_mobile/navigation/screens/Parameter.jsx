51e5f3f3d760ee2e46215cdf33c08b3d873f3047
Mohamed9820m <mohamedhabibmarouani8@gmail.com>
2023-08-07 15:03:22 +0100

    done

--- client_mobile/navigation/screens/Parameter.jsx
+++ client_mobile/navigation/screens/Parameter.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const Param = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>params Screen</Text>
-    </View>
+    <LinearGradient colors={['#001F3F', '#000000']} style={{ flex: 1 }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ color: '#FFFFFF', fontSize: 24 }}>params Screen</Text>
+      </View>
+    </LinearGradient>
   );
 };
 
